1e653d5896f44e0e50d62b327a7e3015d79f4c55
rintan-r <dev.rintanrenatta@gmail.com>
2025-04-25 03:02:57 +0700

    update array

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -6,7 +6,11 @@ const HeroSection = () => {
         { id: "headline_txt", type: "heading", content: `Conference Expense Planner` },
         { id: "subheading_txt", type: "subheading", content: `Plan your next major event with us!`},
         { id: "CTA_label", type:"button", content: `Get Started` },
-        { id: "paragraph_txt", type: "paragraph",  content : `Welcome to BudgetEase Solutions, your trusted partner in simplifying budget management and financial solutions. At BudgetEase, we understand the importance of effective budget planning and strive to provide intuitive, user-friendly solutions to meet the diverse needs of our clients. With a commitment to efficiency and innovation, we empower individuals and businesses to take control of their finances and achieve their goals with ease. At BudgetEase Solutions, our mission is to make budgeting effortless and accessible for everyone. Whether you're a small business owner, a busy professional, or an individual looking to manage your personal finances, we offer tailored solutions to streamline your budgeting process.` },
+        { id: "paragraph_txt", type: "paragraph",  content : [
+            `Welcome to BudgetEase Solutions, your trusted partner in simplifying budget management and financial solutions. At BudgetEase, we understand the importance of effective budget planning and strive to provide intuitive, user-friendly solutions to meet the diverse needs of our clients.`,
+            `With a commitment to efficiency and innovation, we empower individuals and businesses to take control of their finances and achieve their goals with ease. `,
+            `At BudgetEase Solutions, our mission is to make budgeting effortless and accessible for everyone. Whether you're a small business owner, a busy professional, or an individual looking to manage your personal finances, we offer tailored solutions to streamline your budgeting process.`,
+        ]},
     ];
 
     // Helper function
